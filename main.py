a23d45294efbbc3ff9736eb86395eb64e7d107bc
char-lie <webmaster976@gmail.com>
2015-03-31 08:52:37 +0300

    Added command line arguments

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from sys import argv
+
 from src.FrankWolfe import FrankWolfe
 from src.RosenbrockFunction import RosenbrockFunction
 from src.HimmelblauFunction import HimmelblauFunction
@@ -9,37 +11,57 @@ from matplotlib.lines import Line2D
 from matplotlib.colors import LogNorm
 import matplotlib.pyplot as plt
 
-rosenbrock_f = RosenbrockFunction([0, 0])
-himmelblau_f = HimmelblauFunction([0, 0])
-holder_table_f = HolderTableFunction([0, 0])
+functions = list()
+functions.append(RosenbrockFunction([0, 3]))
+functions.append(HimmelblauFunction([0, 0]))
+functions.append(HolderTableFunction([0, 0]))
 
-current_f = holder_table_f
+if __name__ == '__main__':
 
-fw = FrankWolfe(current_f)
-res = fw.find_infimum()
+  function_number = 0
+  accuracy = .25
 
-fig = plt.figure()
-ax = Axes3D(fig, azim = -29, elev = 49)
-X, Y, Z = current_f.get_mesh_XYZ(.5)
-ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.jet)
+  current_f = functions[function_number]
 
-lines_x = list()
-lines_y = list()
-lines_z = list()
-for r in res:
-  lines_x.append(r[0])
-  lines_y.append(r[1])
-  lines_z.append(current_f(r))
-plt.plot(lines_x, lines_y, lines_z, 'kD--', linewidth=2)
+  try:
+    if len(argv) > 1:
+      function_number = int(argv[1])
+      current_f = functions[function_number]
+    if len(argv) > 2:
+      accuracy = float(argv[2])
+  except Exception:
+    print "USAGE: {program} [0<=function_number<={last_func}] [acc]".format(
+          program=argv[0], last_func=len(functions)-1, acc="accuracy")
+    exit(1)
 
-start_point = res[0]
-finish_point = res[-1]
-ax.text(start_point[0], start_point[1], current_f(res[0]), "Start",
-        fontsize=20)
-ax.text(finish_point[0], finish_point[1], current_f(res[-1]), "Finish",
-        fontsize=20)
+  current_f = functions[function_number]
 
-plt.xlabel("x")
-plt.ylabel("y")
-   
-plt.show()
+  fw = FrankWolfe(current_f)
+  res = fw.find_infimum()
+
+  fig = plt.figure()
+  ax = Axes3D(fig, azim = -29, elev = 49)
+  X, Y, Z = current_f.get_mesh_XYZ(accuracy)
+  #ax.plot_surface(X, Y, Z, rstride=1, cstride=1, norm=LogNorm(), cmap=cm.jet)
+  ax.plot_wireframe(X, Y, Z, rstride=1, cstride=1, norm=LogNorm(), cmap=cm.jet)
+
+  lines_x = list()
+  lines_y = list()
+  lines_z = list()
+  for r in res:
+    lines_x.append(r[0])
+    lines_y.append(r[1])
+    lines_z.append(current_f(r))
+  plt.plot(lines_x, lines_y, lines_z, 'kD--', linewidth=2)
+
+  start_point = res[0]
+  finish_point = res[-1]
+  ax.text(start_point[0], start_point[1], current_f(res[0]), "Start",
+          fontsize=20)
+  ax.text(finish_point[0], finish_point[1], current_f(res[-1]), "Finish",
+          fontsize=20)
+
+  plt.xlabel("x")
+  plt.ylabel("y")
+     
+  plt.show()
